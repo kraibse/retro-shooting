bdc0255552a3ed9b6e922f7e8e2e5d088245802c
Tschaggen <chuck.glogowski.cg.dev@gmail.com>
2022-04-11 18:27:31 +0200

    Change canvas sizing method + adding ui layer

--- js/gamemanager.js
+++ js/gamemanager.js
@@ -1,12 +1,27 @@
 const vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0)
 const vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0)
 
+const viewportScalingfactor = 0.5625;
+
 function setup()
 {
-    var canvas = createCanvas(vw - 16, 600);
-    canvas.parent("canvas");
+    setSize();
 }
 
 function draw() {
-    background(0);
+    background(125);
+}
+
+function setSize() {
+    
+    let renderWidth = vh * viewportScalingfactor;
+
+    if(renderWidth > vw) {
+        renderWidth = vw;
+    }
+
+    var canvas = createCanvas(renderWidth, vh);
+    canvas.parent("canvas");
+
+    select('#ui').size(renderWidth, vh);
 }

cf97b8316e3060898d3bcc8906d9bf4484c7aeab
kraibse <33869366+kraibse@users.noreply.github.com>
2023-02-08 09:51:07 +0100

    Added bullet collision detection+changed posx,posy

--- js/bullet.js
+++ js/bullet.js
@@ -1,28 +1,69 @@
 class Bullet {
-    posx = 0;
-    posy = 0;
+    x = 0;
+    y = 0;
 
     speed = 10;
+    size = 10;
 
-    constructor(x=0, y=0, damage=1, dispersion=0) {
-        this.posx = x;
-        this.posy = y;
+    constructor(x = -100, y = 0, damage = 1, dispersion = 0) {
+        this.x = x;
+        this.y = y;
 
         this.damage = damage;
         this.dispersion = dispersion;
+    }
 
-        this.render();
+    checkCollisions() {
+        gm.enemies.forEach(enemy => {
+            if (!enemy.isOnScreen()) {
+                return;
+            }
+
+            let distanceVector = p5.Vector.sub(enemy.getPos(), this.getPos());
+            let mag = distanceVector.mag();
+            let minDistance = this.size + enemy.size;
+            // let minDistance = 100;
+
+            if (mag < minDistance) {
+                console.log("hit");
+
+                stroke(0);
+                line(this.x, this.y - 30, enemy.x, enemy.y);
+
+                enemy.respawn();
+                // gm.enemies.splice(gm.enemies.indexOf(enemy), 1);
+
+                // enemy.health -= this.damage;
+            }
+
+        });
+    }
+
+    getPos() {
+        return new p5.Vector(this.x, this.y);
     }
 
     move() {
-        this.posx += this.dispersion;
-        this.posy += (-1) * this.speed;
+        this.x += this.dispersion;
+        this.y += (-1) * this.speed;
 
+        // this.checkCollisions();
         this.render();
     }
 
     render() {
         stroke(0);
-        circle(this.posx, this.posy - 30, 10);
+        circle(this.x, this.y - 30, this.size);
+    }
+
+    setPos(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    setProperties(size=this.size, damage=this.damage, dispersion=this.dispersion) {
+        this.size = size;
+        this.damage = damage;
+        this.dispersion = dispersion;
     }
 }
